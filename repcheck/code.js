574d6a17409cb1759cc87b158214629997603b60
Max Nicula <max@btcxchange.net>
2018-01-15 19:04:01 +0200

    change logo and text

--- repcheck/code.js
+++ repcheck/code.js
@@ -2,7 +2,7 @@
 
 function checker() { // eslint-disable-line
   const name = $('#name')[0].value.replace(/[^A-Za-z0-9_]/gi, '');
-  const validNames = ['EtherDeltaZack_twitter', 'EtherDeltaRep1_twitter', 'EtherDeltaRep2_twitter', 'EtherDeltaRep3_twitter', 'EtherDeltaRep4_twitter', 'EtherDeltaUX_twitter'];
+  const validNames = ['Ethermium'];
   if (validNames.map(x => x.toUpperCase()).indexOf(name.toUpperCase()) >= 0) {
     $('#result').html(`${name.toUpperCase()} is an official EtherDelta representative.`);
   } else {
